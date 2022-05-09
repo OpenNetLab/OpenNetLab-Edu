d8b193996c81972cfd1ede61e1e7766e3d5e210b
zdszero <1335908147@qq.com>
2022-05-09 16:06:53 +0800

    refactor(gbn): receiver self.log and self.record

--- examples/gbn_sender/gbn_receiver.py
+++ examples/gbn_sender/gbn_receiver.py
@@ -1,11 +1,12 @@
 import asyncio
 import json
 import random
-import time
+from datetime import datetime
 
 from OpenNetLab.node.server import TCPServerNode
 from gbn_packet import new_packet
 from gbn_logger import logger
+
 
 class GBNReceiver(TCPServerNode):
     async def setup(self):
@@ -22,37 +23,37 @@ class GBNReceiver(TCPServerNode):
             self.message = ''
             self.failed_test = []
             self.success_test = []
-            self.recorder.set_headers(('time', 'absno', 'seqno', 'message', 'status'))
+            self.recorder.set_headers(
+                ('time', 'absno', 'seqno', 'message', 'status'))
+            self.verbose = False
 
     async def recv_callback(self, data):
         if data['absno'] < len(self.message):
             pkt = new_packet(0, 0, (data['absno'] + 1) % self.seqno_range, '')
             await self.send(pkt)
-            logger.info('[ACK]: Sending ackno %d on received message %s' % (self.next_seqno, self.message[data['absno']]))
-            self.recorder.add_record(self.test_idx, (time.strftime('%H:%M:%S'), data['absno'], self.next_seqno, data['message'], 'DUP'))
+            await self.log('ACK', data)
+            await self.record('DUP', data)
             return
         if data['seqno'] != self.next_seqno:
-            logger.warning('[DISCARD]: Invalid seqno %d, expected %d' % (data['seqno'], self.next_seqno))
-            self.recorder.add_record(self.test_idx, (time.strftime('%H:%M:%S'), data['absno'], self.next_seqno, data['message'], 'DISCARD'))
+            await self.log('DISCARD', data)
+            await self.record('DISCARD', data)
             return
         if self.is_loss():
-            logger.warning('[LOST]: seqno %d on message %s is lost' % (data['seqno'], data['message']))
-            self.recorder.add_record(self.test_idx, (time.strftime('%H:%M:%S'), data['absno'], self.next_seqno, data['message'], 'LOST'))
+            await self.log('LOST', data)
+            await self.record('LOST', data)
             return
         self.message += data['message']
-        self.recorder.add_record(self.test_idx, (time.strftime('%H:%M:%S'), data['absno'], self.next_seqno, data['message'], 'RCVD'))
+        await self.record('RCVD', data)
         self.next_seqno = (self.next_seqno + 1) % self.seqno_range
         pkt = new_packet(0, 0, self.next_seqno, '')
-        # print(self.message)
         if not self.is_loss():
             await self.send(pkt)
-            logger.info('[ACK]: Sending ackno %d on message %s' % (self.next_seqno, data['message']))
-
+            await self.log('ACK', data)
 
     async def evaulate_testcase(self):
         assert self.test_idx < len(self.testcases)
         expected_msg = self.testcases[self.test_idx]
-        print(self.message)
+        # print(self.message)
         if expected_msg == self.message:
             self.success_test.append(self.test_idx)
             print('TESTCASE %d PASSED' % self.test_idx)
@@ -64,12 +65,30 @@ class GBNReceiver(TCPServerNode):
         self.next_seqno = 0
 
     async def teardown(self):
-        logger.info('[TEST CASES]: %d/%d PASSED' % (len(self.success_test), len(self.testcases)))
-        logger.info('PASSED TESTS: %s' % self.success_test)
-        logger.info('FAILED TESTS: %s' % self.failed_test)
+        print('[TEST CASES]: %d/%d PASSED' %
+              (len(self.success_test), len(self.testcases)))
+        print('PASSED TESTS: %s' % self.success_test)
+        print('FAILED TESTS: %s' % self.failed_test)
 
     def is_loss(self):
         return random.random() < self.loss_rate
+
+    async def log(self, act, data):
+        if not self.verbose:
+            return
+        if act == 'ACK':
+            logger.info('[ACK]: Sending ackno %d on received message %s' % (
+                self.next_seqno, self.message[data['absno']]))
+        elif act == 'DISCARD':
+            logger.warning('[DISCARD]: Invalid seqno %d, expected %d' % (
+                data['seqno'], self.next_seqno))
+        elif act == 'LOST':
+            logger.warning('[LOST]: seqno %d on message %s is lost' %
+                           (data['seqno'], data['message']))
+
+    async def record(self, act, data):
+        self.recorder.add_record(self.test_idx, (datetime.now().strftime(
+            '%H:%M:%S.%f'), data['absno'], self.next_seqno, data['message'], act))
 
 
 async def main():
