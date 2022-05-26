f63b502c6b9f5bf41076983a64665c5449db92fe
zdszero <1335908147@qq.com>
2022-05-26 14:35:04 +0800

    feat(timer): callback with parameters

--- OpenNetLab/utils/timer.py
+++ OpenNetLab/utils/timer.py
@@ -1,17 +1,21 @@
 import asyncio
 
 class Timer:
-    def __init__(self, timeout, callback):
+    def __init__(self, timeout, callback, *args):
         """Set a timer a task. After the timeout seconds,
         callback will be executed
         """
         self.timeout = timeout
         self.callback = callback
+        self.args = args
         self.task = None
+        self.auto_reset = False
 
     async def _job(self):
         await asyncio.sleep(self.timeout / 1000)
-        await self.callback()
+        await self.callback(*self.args)
+        if self.auto_reset:
+            self.reset()
 
     def cancel(self):
         """Cancel the task
