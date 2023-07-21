0d28ca9afdcd217d9f3347652b6d1814863b0f04
zdszero <1335908147@qq.com>
2023-07-21 22:04:07 +0800

    add real time simulation in sim

--- OpenNetLab/simpy/__init__.py
+++ OpenNetLab/simpy/__init__.py
@@ -1,9 +1,10 @@
 from .core import Environment
 from .exceptions import SimPyException, Interrupt, StopProcess
 from .events import Event, Timeout, Process, AllOf, AnyOf
+from .rt import RealtimeEnvironment
 
 __all__ = [
-    Environment,
+    Environment, RealtimeEnvironment,
     Event, Timeout, Process, AllOf, AnyOf, Interrupt,
     SimPyException, Interrupt, StopProcess,
 ]
