ae5de8effabd0d50503189ec04345b1a9c6e1bd0
zdszero <1335908147@qq.com>
2023-07-31 15:16:42 +0000

    delete old labs and unused lib module

--- examples/sr_sender/sr_packet.py
+++
@@ -1,7 +0,0 @@
-def new_packet(absno: int, seqno: int, ackno: int, message: str):
-    return {
-        'absno': absno,
-        'seqno': seqno,
-        'ackno': ackno,
-        'message': message
-    }
